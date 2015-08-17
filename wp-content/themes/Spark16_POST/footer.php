1054c699356fd9707b30984a0a372cdc4e882eb5
Damon <damonjhastins@gmail.com>
2015-08-17 14:47:15 -0700

    Map Panels

--- wp-content/themes/Spark16_POST/footer.php
+++ wp-content/themes/Spark16_POST/footer.php
@@ -2,15 +2,6 @@
 <?php 
 	$context = Timber::get_context();
 
-	$qlContext = array("nav"=>new TimberMenu('Quick Links'));
-	$context['quick_links'] = Timber::compile('views/components/nav.twig', $qlContext);
-	$context['logos'] = get_field('Logos', 'option');
-	$context['facebook'] = get_field('facebook', 'option');
-	$context['twitter'] = get_field('twitter', 'option');
-	$context['linked_in'] = get_field('linked_in', 'option');
-	$context['stay_connected'] = do_shortcode( '[contact-form-7 id="303" title="Newsletter Subscribe"]' );
-	$context['rates'] = get_field('rate_group', 'option');
-
 	Timber::render('view/section/footer.twig', $context);
 
 	wp_footer(); 
